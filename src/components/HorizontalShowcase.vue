5d3f9716f3fcd7f90b055d8a0553904893b0ceb8
mohamed Ali <drame.ibou.07@gmail.com>
2026-05-01 15:25:12 +0200

    Animate marquee strip with infinite horizontal scroll

--- src/components/HorizontalShowcase.vue
+++ src/components/HorizontalShowcase.vue
@@ -5,8 +5,13 @@ import { siteProfile } from '@/data/projects'
 <template>
   <section class="marquee-strip page-block" data-page-intro aria-label="Bandeau expertise">
     <div class="marquee-strip__viewport">
-      <div class="marquee-strip__track">
+      <div class="marquee-strip__track" aria-hidden="true">
         <span v-for="item in siteProfile.marquee" :key="item" class="marquee-strip__item">
+          {{ item }}
+          <i aria-hidden="true">✦</i>
+        </span>
+        <!-- Duplicate for seamless loop -->
+        <span v-for="item in siteProfile.marquee" :key="'dup-' + item" class="marquee-strip__item">
           {{ item }}
           <i aria-hidden="true">✦</i>
         </span>
